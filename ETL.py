a3f0140a5fa0a37c33c010de3c31044060e21853
Millerz8 <joe.h.miller@wustl.edu>
2026-04-07 13:14:25 -0400

    Japan

--- ETL.py
+++ ETL.py
@@ -1,9 +1,41 @@
 import requests
+import numpy as np
 import pandas as pd
 import io
 response = requests.get('https://www.tourism.jp/wp/wp-content/uploads/2026/03/JTM_inbound_20260304eng.xlsx')
 df = pd.read_excel(io.BytesIO(response.content))
 df = df.iloc[2:]
-df.columns = ["Month", "Visitors", "%Change", "Notes"]
-df.drop_index()
+df.columns = ["Month", "Total Visitors", "%Change", "Notes"]
+df = df.reset_index()
+df.drop(columns = ["index"], inplace = True)
+
+mindex = df[df["Total Visitors"].isna()].index.min() # Making sure null data is not represented
+df = df.iloc[:mindex]
 print(df)
+full_cycle = int((df.shape[0] / 48))
+full_cycle_remainder = df.shape[0] % 48
+days_leap = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+days_nonleap = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+days = days_leap + days_nonleap * 3
+total_days = days * full_cycle + days[:full_cycle_remainder] # change based on number of yeas
+print(total_days)
+df["Days"] = total_days
+
+
+df["Month"] = df["Month"].str.rstrip()
+df["Year"] = df["Month"].str[:-4]
+df["Month"] = df["Month"].str[-4:-1]
+df['Year'] = df['Year'].ffill()
+ # Re-ordering columns
+df["Total Visitors"] = df["Total Visitors"].astype(int)
+df["Year"].replace("", np.nan, inplace = True)
+df["Year"] = df["Year"].ffill()
+df["Date"] = df["Month"] + " " + df["Year"]
+df["Date"] = df["Date"].str.strip()
+df["Date"] = pd.to_datetime(df["Date"])
+df["Daily Visitors"] = df["Total Visitors"]/df["Days"]
+df = df[["Year", "Month", "Days", "Date", "Total Visitors", "Daily Visitors", "%Change", "Notes"]]
+
+print(print(df.to_string()))
+
+
